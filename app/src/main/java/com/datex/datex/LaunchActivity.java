1f2c1b78f32407fa82f65365525a310288ba9ab6
Francis-PC\Francis <francis94c@gmail.com>
2018-04-09 21:11:51 +0100

    Initial Commit.

---
+++ app/src/main/java/com/datex/datex/LaunchActivity.java
@@ -0,0 +1,12 @@
+package com.datex.datex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

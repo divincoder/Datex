d59db210fb7dfc115d1c13ff3374afb3868d82e5
Francis-PC\Francis <francis94c@gmail.com>
2018-05-29 19:50:10 +0100

    Refactor Diagnosis Object.

--- app/src/main/java/com/datex/datex/model/Diagnosis.java
+++ app/src/main/java/com/datex/datex/model/Diagnosis.java
@@ -4,17 +4,13 @@ import android.content.ContentValues;
 
 import com.datex.datex.model.DatexDBContract.*;
 
-public class Diagnosis implements DatabaseObject {
+public class Diagnosis {
 
     private int id;
     private String name;
     private ValidationListener listener;
 
-    public Diagnosis(String name) {
-        this.name = name;
-    }
-
-    public Diagnosis(int id, String name) {
+    Diagnosis(int id, String name) {
         this.id = id;
         this.name = name;
     }
@@ -23,62 +19,8 @@ public class Diagnosis implements DatabaseObject {
         return name;
     }
 
-    @Override
-    public ContentValues getContentValues() {
-        ContentValues cv = new ContentValues();
-        cv.put(DBContract.getName(DiagnosisTable.NAME), name);
-        return cv;
-    }
-
-    @Override
-    public String getTableName() {
-        return DiagnosisTable.TABLE_NAME;
-    }
-
-    @Override
-    public String getPrimaryKey() {
-        return DBContract.getName(DiagnosisTable.ID);
-    }
-
-    @Override
     public int getId() {
         return id;
     }
 
-    @Override
-    public void setField(String field, int value) {
-
-    }
-
-    @Override
-    public void setField(String field, String value) {
-
-    }
-
-    @Override
-    public String getField(String field) {
-        return null;
-    }
-
-    @Override
-    public boolean validateNulls() {
-        boolean valid = true;
-        if (name == null) {
-            valid = false;
-            if (listener != null) {
-                listener.onCatchNull(DB_INPUT.NAME);
-            }
-        } else if (name.equals("") || name.equals("N")) {
-            valid = false;
-            if (listener != null) {
-                listener.onCatchNull(DB_INPUT.NAME);
-            }
-        }
-        return valid;
-    }
-
-    @Override
-    public void setValidationListener(ValidationListener listener) {
-        this.listener = listener;
-    }
 }
